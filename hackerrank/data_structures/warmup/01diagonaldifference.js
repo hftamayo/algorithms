19287e32d117c4888d97cab78bf1a3c89ebe5daf
hftamayo <hftamayo@gmail.com>
2022-07-08 09:57:57 -0600

    diagonal difference javascript

--- hackerrank/data_structures/warmup/01diagonaldifference.js
+++ hackerrank/data_structures/warmup/01diagonaldifference.js
@@ -2,3 +2,27 @@
 https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=false
 */
 
+function processData(input) {
+    var rightDia = 0;
+    var leftDia = 0;
+    var inputArray = input.split("\n");
+    var squareSize = parseInt(inputArray.shift() - 1);
+    for (var i = 0; i < inputArray.length; i++) {
+        var row = inputArray[i].split(" ");
+        rightDia += parseInt(row[i]);
+        leftDia += parseInt(row[squareSize]);
+        squareSize--;
+    }
+    console.log(Math.abs(rightDia - leftDia));
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
